17630068f7e41ac8adc4984590ad90bf23c5056e
Mashina2 <blknigga@mail.ru>
2023-06-16 16:04:13 +0300

    v4

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ data = "0000"
 
 
 def send_data(data):
-    tmp = str.encode(data)
+    '''tmp = str.encode(data)'''
     try:
        '''udp_socket.sendto(tmp, address)'''
     except Exception as e:
